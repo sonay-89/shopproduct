fe66ba64c0f3180598e6b251a7dfcc37982d824c
Sofia <anay15051989@gmail.com>
2024-11-12 23:08:24 -0300

    Все мои изменения

--- ShopProduct/products/models.py
+++ ShopProduct/products/models.py
@@ -1,25 +1,27 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from django.db import models
-
 from ShopProduct.models import TimestampedModel
 from ShopProduct.store.models import Store
 
 
 class Product(TimestampedModel):
-    name = models.CharField(max_length=100) #
+    product_name = models.CharField(max_length=100, unique=True) #
     price = models.DecimalField(max_digits=20, decimal_places=2)
-    # store = models.ManyToManyField('store.Store',through='StoreProductCount')
 
     def __str__(self):
-        return self.name
+        return self.product_name
 
 class StoreProductCount(TimestampedModel):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_name = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='store_counts')
+    store_name = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='product_counts')
     count = models.PositiveIntegerField()
-    store = models.ForeignKey('store.Store', on_delete=models.CASCADE)
-    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='products', null=True, blank=True)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='product_counts')
+    store_price = models.DecimalField(max_digits=20, decimal_places=2, editable=False, default=0)
 
-    @property
-    def price(self):
-        return self.product.price
+    def save(self, *args, **kwargs):
+        # Рассчитываем store_price на основе цены продукта и наценки магазина
+        self.store_price = self.product_name.price + (self.product_name.price * self.store_name.markup_percentage / 100)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.product_name} - {self.count} (Store: {self.store_name})"
